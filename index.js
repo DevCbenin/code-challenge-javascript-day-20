55084b68c3bb9497860598290d556394c3668dcb
Samson Ehigiator <samson.ehigiator@physci.uniben.edu>
2020-05-25 12:52:45 +0100

    code challenge day 20

---
+++ index.js
@@ -0,0 +1,12 @@
+
+class Index {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  find() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
+
+export default Index;
